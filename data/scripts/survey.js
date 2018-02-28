c0c37d33d63b7c526358748781cb830a4e25ce7d
Kalindrov <34249113+Kalindrov@users.noreply.github.com>
2018-02-27 23:49:51 -0600

    updated files

--- data/scripts/survey.js
+++ data/scripts/survey.js
@@ -1,8 +1,19 @@
-$('#submit').on('click'), function() {
-    $("#friends").validate();
-}
+$('#submit').submit(function(event) {
+    $('#form-data').validate();
 
+    var formData = $('#form-data').serialize();
+    // $.ajax({
+    //     url: '/api/friends',
+    //     type: 'post',
+    //     dataType: 'json',
+    //     data: formData,
+    //     success: function () {
+    //         console.log("does it work?");
+    //     }
+    // });
 
-
-
-
+    // here we are going to push the data from the form submission
+    // name, photo url, quest1 thru quest10 should be stored into the friendArray
+    // then the scores from each question is totalled to find the closest friend near the total
+    // this will then display your new best friend in friend.html
+})
